fa232c89bb87b01491ccb580e094954d5f7953a4
Diego Acer <diegomanuelmfq@outlook.com>
2023-05-12 15:49:28 -0500

    Add components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,48 +1,17 @@
-import { FormEvent, ChangeEvent } from "react";
-import { Region } from "@/types/country";
 import styles from "./Home.module.css";
-import { ListOfCountrys } from "@/components/Countrys";
-import { useSearch } from "@/hooks/useSearch";
+import { CountrySearch } from "@/components/Country";
 import { useCountryStore } from "@/store/useCountry";
-
-const regions = Object.values(Region);
+import { Form } from "@/components/Form";
 
 export const Home = () => {
-  const { search, setSearch } = useSearch();
   const countrySearched = useCountryStore((store) => store.countrySearched);
-  const getCountryByName = useCountryStore((store) => store.getCountryByName);
-  const setCountries = useCountryStore((store) => store.setCountries);
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      if (search === "") return;
-      getCountryByName(search);
-    } catch (error) {
-      throw new Error("Error seaching country");
-    }
-  };
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} value={search} />
-        <select name="Filter by Region">
-          {regions.map((region) => (
-            <option key={region} value={region}>
-              {region}
-            </option>
-          ))}
-        </select>
-      </form>
-
+      <Form />
+      
       <div className={styles.cards}>
-        <button onClick={setCountries}>Volver a Countries</button>
-        <ListOfCountrys countries={countrySearched} /> {/* [{..peru}] */}
+        <CountrySearch countries={countrySearched} /> {/* [{..peru}] */}
       </div>
     </>
   );
