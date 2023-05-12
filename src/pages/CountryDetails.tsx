f44fa066ff3c418e1a9ab6ad05c7458d8493d371
Diego Acer <diegomanuelmfq@outlook.com>
2023-05-12 15:46:06 -0500

    Moved add back button

--- src/pages/CountryDetails.tsx
+++ src/pages/CountryDetails.tsx
@@ -2,12 +2,15 @@ import { useRoute } from "wouter";
 import { Country } from "@/types/country";
 import { useEffect, useState } from "react";
 import { useCountryStore } from "@/store/useCountry";
+import Button from "@mui/material/Button";
 
 export const CountryDetails = () => {
   const [, params] = useRoute("/country/:code");
   const [country, setCountry] = useState<Country[]>([]);
   const getCountryByCode = useCountryStore((state) => state.getCountryByCode);
   const countries = useCountryStore((state) => state.countries);
+  const setCountries = useCountryStore((store) => store.setCountries);
+
 
   useEffect(() => {
     const countryDetails = getCountryByCode(params?.code);
@@ -17,6 +20,7 @@ export const CountryDetails = () => {
   return (
     <div>
       CountryDetails: {params?.code}
+      <Button variant="contained" onClick={setCountries}>Volver a Countries</Button>
       <h1>{country[0]?.name?.common}</h1>
     </div>
   );
